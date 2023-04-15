1a0356a3af1e0b890bd54261e2f2ae00462a378c
JungHaeSung <gotjd0230@naver.com>
2023-04-15 17:09:59 +0900

    변경

--- number/script.js
+++ number/script.js
@@ -1,96 +1,107 @@
-const $3dgt = document.querySelector('#dgt3');
-const $4dgt = document.querySelector('#dgt4');
-const $5dgt = document.querySelector('#dgt5');
-const digits = [$3dgt, $4dgt, $5dgt];
+const $container = document.querySelector('.container');
+const $digit = document.querySelector('.digit');
 const $numberInput = document.querySelector('.number-input');
 const $numberInputButton = document.querySelector('.number-input-button');
 const $table = document.querySelector('.table');
-const $nth1tr = document.querySelector('.nth1tr');
-const $nth2tr = document.querySelector('.nth2tr');
-const $nth3tr = document.querySelector('.nth3tr');
-const $nth4tr = document.querySelector('.nth4tr');
-const $nth5tr = document.querySelector('.nth5tr');
-const $nth6tr = document.querySelector('.nth6tr');
-const $nth7tr = document.querySelector('.nth7tr');
-const $nth8tr = document.querySelector('.nth8tr');
-const nthtrs = [$nth1tr, $nth2tr, $nth3tr, $nth4tr, $nth5tr, $nth6tr, $nth7tr, $nth8tr]
 
-
-let nowDigit = 3;
-$3dgt.addEventListener('click', (e) => {
-        nowDigit = 3;
-        digits[0].classList.add('active');
-        digits[1].classList.remove('active');
-        digits[2].classList.remove('active');
-})
-$4dgt.addEventListener('click', (e) => {
-        nowDigit = 4;
-        digits[0].classList.remove('active');
-        digits[1].classList.add('active');
-        digits[2].classList.remove('active');
-})
-$5dgt.addEventListener('click', (e) => {
-        nowDigit = 5;
-        digits[0].classList.remove('active');
-        digits[1].classList.remove('active');
-        digits[2].classList.add('active');
-})
-
-
-let numbers;
-let randNum = Math.floor(10000000000 * Math.random()).toString().split('');
-let randNoDup = [...new Set(randNum)]
-let homerunNumber = [];
-for (let i = 0; i < nowDigit; i++) {
-        homerunNumber.push(randNoDup[i]);
+for (let i = 0; i < 3; i++) {
+    let newDigitButton = document.createElement('button');
+    newDigitButton.classList.add('digit-button');
+    newDigitButton.textContent = `${i+3}자리`;
+    $digit.appendChild(newDigitButton);
 }
-let tryCount = 0;
-console.log(homerunNumber);
+
+function lotteryHomerunNumber() {
+    let randNumArr = Math.floor(100000000000 * Math.random()).toString().split('');
+    let randNoDupArr = [...new Set(randNumArr)]
+    homerunNumberArr = randNoDupArr.splice(0, nowDigit);
+    tryCount = 0;
+    console.log("정답은", homerunNumberArr.join(''));
+}//정답 숫자를 추첨
+
+function getIndex(element) {
+    console.log(element.parentNode.childNodes);
+    for(var i = 0; i < element.parentNode.childNodes.length; i++) {
+        if (element.parentNode.childNodes[i+1] === element) {
+            return i+3;
+        }
+    }
+}
+
+function handleDigitButton(e) {
+    nowDigit = getIndex(e.target);
+    e.target.parentNode.childNodes.forEach(element => {
+        if (element.classList && element.classList.contains('active')) {
+            element.classList.remove('active')
+        }
+    });
+    e.target.classList.add('active');
+    inputValueNumbers = e.target.value;
+    lotteryHomerunNumber();
+}//자릿수 버튼 이용 시 함수
 
 function handleOnInput(e, maxlength) {
-        var value = e.target.value;
-        if (isNaN(value[value.length-1])) {
-                e.target.value = value.substring(0, value.length - 1);
-        }
-        if (value.length > maxlength)  {
-                e.target.value = value.substr(0, maxlength);
-        }
-}
-let a =['1', '2', '3', '4', '5']
-let b =['3', '4', '5']
+    var value = e.target.value;
+    if (isNaN(value[value.length-1])) {
+        e.target.value = value.substring(0, value.length - 1);
+    }
+    if (value.length > maxlength)  {
+        e.target.value = value.substr(0, maxlength);
+    }
+}//숫자 입력 칸 문자 금지, 자릿수 제한
 
-function handleNumbers(numbers) {
-        var logNumber = numbers;
-        var ballNum = 0;
-        var strikeNum = 0;
-        var compareNum = numbers.toString().split('');
-        for (var i = 0; i < nowDigit; i++) {
-                for (var j = 0; j < nowDigit; j++) {
-                        if (homerunNumber[i].match(compareNum[j])) {
-                                ballNum++;
-                        }
-                }
-                if (compareNum[i] === homerunNumber[i]) {
-                        strikeNum++;
-                }
+function handleNumbers(tryNumbers) {
+    var logNumber = tryNumbers;
+    if (logNumber.length != nowDigit) {
+        alert(`${nowDigit}자를 입력해주세요!`);
+        return;
+    }
+    $numberInput.value = null;
+    var ballNum = 0;
+    var strikeNum = 0;
+    var compareNum = tryNumbers.toString().split('');
+    for (var i = 0; i < nowDigit; i++) {
+        for (var j = 0; j < nowDigit; j++) {
+            if (homerunNumberArr[i] == compareNum[j]) {
+                ballNum++;
+            }
         }
-        if (strikeNum) {
-                ballNum = ballNum - strikeNum;
+        if (homerunNumberArr[i] == compareNum[i]) {
+            strikeNum++;
         }
-        var logResult = `B: ${ballNum}, S: ${strikeNum}`;
-        nthtrs[tryCount].childNodes[3].innerHTML = logNumber;
-        nthtrs[tryCount].childNodes[5].innerHTML = logResult;
-        if (strikeNum == 3) {
-                alert('축하드립니다!');
-        }
-        tryCount++;
+    }
+    ballNum = ballNum - strikeNum;
+    var logResult = `B: ${ballNum}, S: ${strikeNum}`;
+    $table.childNodes[1].childNodes[tryCount*2+2].childNodes[3].innerHTML = logNumber;
+    $table.childNodes[1].childNodes[tryCount*2+2].childNodes[5].innerHTML = logResult;
+    if (strikeNum == 3) {
+        alert('축하드립니다!');
+    }
+    tryCount++;
+}//입력 누를 시 숫자 시도
+
+let nowDigit = 3;
+$digit.childNodes[(nowDigit-3)*2+1].classList.add('active');
+let inputValueNumbers;
+let homerunNumberArr;
+let tryCount;
+lotteryHomerunNumber();
+
+for (let i = 0; i < 3; i++) {
+    $digit.childNodes[i+1].addEventListener('click', (e) => {handleDigitButton(e)});
 }
 
-$numberInput.addEventListener('keyup', (e) => {
-        numbers = e.target.value;
-        handleOnInput(e, nowDigit);
-});//자릿수 제한
+$numberInput.addEventListener('keyup' , (e) => {
+    inputValueNumbers = e.target.value;
+    handleOnInput(e, nowDigit);
+});
+
+$numberInput.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+        handleNumbers(inputValueNumbers);
+    }
+}); 
 
 $numberInputButton.addEventListener('click', (e) => {
-        handleNumbers(numbers);
-})
+    handleNumbers(inputValueNumbers);
+});
