fcd761c532b6cff3ad738119b5af6b08e632ba7a
JungHaeSung <gotjd0230@naver.com>
2023-05-04 17:23:07 +0900

    dom 실습

---
+++ dom/script.js
@@ -0,0 +1,37 @@
+const $root = document.querySelector("#root");
+const $bag = document.querySelector(".shopping-bag");
+const $box = document.querySelector(".item-box");
+
+const itemAmount = 30;
+
+function returnTarget(event) {
+    let target = event.target;
+    if (!event.target.id) {
+        target = event.target.parentNode;
+    }
+    return target;
+}
+
+function appendItem(target) {
+    let from = target.parentNode;
+    let to;
+    if (from == $bag) {
+        to = $box
+    } else if (from == $box) {
+        to = $bag
+    }
+    to.append(target);
+}
+
+var item = document.createElement("span");
+item.className = "item";
+for (var i = 1; i <= itemAmount ; i++) {
+    let tempNode = item.cloneNode(true);
+    tempNode.addEventListener("click", (e) => {
+        let target = returnTarget(e);
+        appendItem(target);
+    });
+    tempNode.innerHTML = `<img src="/temp-repository/dom/shoes/${i}.jpg">`
+    tempNode.id = `item-${i}`;
+    $box.append(tempNode);
+}
