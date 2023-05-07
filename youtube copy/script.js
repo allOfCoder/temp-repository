7ab778f385a72941b1efefed2fb2459a150de4ec
JungHaeSung <gotjd0230@naver.com>
2023-05-07 13:05:27 +0900

    틀

--- youtube copy/script.js
+++ youtube copy/script.js
@@ -1,1 +1,12 @@
 const $root = document.querySelector(".root");
+
+const div = document.createElement("div");
+function createDivElement(elemName, className) {
+    tempElement = div.cloneNode(false);
+    tempElement.className = className;
+    return tempElement;
+}
+const $headerSet = createDivElement("div", "header-set");
+const $contentSet = createDivElement("div", "content-set");
+
+$root.append($headerSet, $contentSet);
