fd44b26cf9bf67a138337864fcc04e1962a65c60
JungHaeSung <gotjd0230@naver.com>
2023-05-17 11:01:05 +0900

    실습3

--- login/script.js
+++ login/script.js
@@ -1,15 +1,16 @@
-const loginButton = document.querySelector('#loginButton');
-const signupIdInput = document.querySelector('.id');
-const signupPwInput = document.querySelector('.pw');
-const signupPwCheckInput = document.querySelector('.pw-check');
-const signupStudentNumberInput = document.querySelector('.student-number');
-const signupMajor = document.querySelector('.major');
-const signupButton = document.querySelector('.signup');
-const signupCancelButton = document.querySelector('.cancel');
-const signupPhoneNumberInput = document.querySelector('.phone-number');
+const $loginButton = document.querySelector('#loginButton');
+const $signupIdInput = document.querySelector('.id');
+const $signupPwInput = document.querySelector('.pw');
+const $signupPwCheckInput = document.querySelector('.pw-check');
+const $signupStudentNumberInput = document.querySelector('.student-number');
+const $signupMajor = document.querySelector('.major');
+const $signupButton = document.querySelector('.signup');
+const $signupCancelButton = document.querySelector('.cancel');
+const $signupPhoneNumberInput = document.querySelector('.phone-number');
+const $catImage = document.querySelector('.cat-image');
 
-if (loginButton) {
-    loginButton.addEventListener('click', (e) => {
+if ($loginButton) {
+    $loginButton.addEventListener('click', (e) => {
         var id = e.target.parentNode.childNodes[1].childNodes[1].value;
         var pw = e.target.parentNode.childNodes[1].childNodes[3].value;
         if (id == '' || pw == '') {
@@ -19,7 +20,7 @@ if (loginButton) {
     //로그인버튼을 누르면 아이디 비번에 문자 있는 것 확인하고 없으면 alert
 }
 
-if (signupButton) {
+if ($signupButton) {
     function formatCheck(inputDocument, format) {
         var format = format;
         var formatRight;
@@ -56,9 +57,9 @@ if (signupButton) {
     }//학번에 숫자만 들어가게 하고 maxlength자로 제한하는 함수
     function checkMajor(e) {
         if (e.target.value.match(new RegExp('....136...'))) {
-            signupMajor.innerHTML = '컴퓨터공학부';
+            $signupMajor.innerHTML = '컴퓨터공학부';
         } else if (e.target.value.match(new RegExp('....120...'))) {
-            signupMajor.innerHTML = '기계공학부';
+            $signupMajor.innerHTML = '기계공학부';
         }
     }//학번을 보고 학부를 major에 추가하는 함수
     function checkPhoneNember (e) {
@@ -66,36 +67,41 @@ if (signupButton) {
         e.target.value = String(phoneNumber).replace(/(^01.{1})([0-9]{4})([0-9]+)/, '$1-$2-$3');
     }//전화번호를 형식에 맞게 바꿔주는 함수
 
-    formatCheck(signupIdInput, new RegExp('.@.'));
-    formatCheck(signupPwInput, new RegExp('........'));
-    formatCheck(signupPwCheckInput, null);//정규식 파라미터자리에 null을 넣으면 비밀번호 확인하는 함수
-    formatCheck(signupStudentNumberInput, new RegExp('20........'));
-    signupStudentNumberInput.addEventListener('keyup', (e) => {handleOnInput(e, 10)});
-    signupStudentNumberInput.addEventListener('change', (e) => {checkMajor(e)});
-    signupPhoneNumberInput.addEventListener('keyup', (e) => {handleOnInput(e, 13)});
-    signupPhoneNumberInput.addEventListener('blur', (e) => {checkPhoneNember(e)});
+    formatCheck($signupIdInput, new RegExp('.@.'));
+    formatCheck($signupPwInput, new RegExp('........'));
+    formatCheck($signupPwCheckInput, null);//정규식 파라미터자리에 null을 넣으면 비밀번호 확인하는 함수
+    formatCheck($signupStudentNumberInput, new RegExp('20........'));
+    $signupStudentNumberInput.addEventListener('keyup', (e) => {handleOnInput(e, 10)});
+    $signupStudentNumberInput.addEventListener('change', (e) => {checkMajor(e)});
+    $signupPhoneNumberInput.addEventListener('keyup', (e) => {handleOnInput(e, 13)});
+    $signupPhoneNumberInput.addEventListener('blur', (e) => {checkPhoneNember(e)});
 
-    // signupButton.addEventListener('click', (e) => {
-    //     var canSignup = {
-    //         0: signupIdInput.style.borderBottomColor == 'black',
-    //         1: signupPwInput.style.borderBottomColor == 'black',
-    //         2: signupPwCheckInput.style.borderBottomColor == 'black',
-    //         3: signupStudentNumberInput.style.borderBottomColor == 'black'
-    //     };
-    //     for (var i = 0; i < canSignup.length; i++) {
-    //         if (!canSignup.i) {
-    //             console.log(!canSignup.i)
-    //             alert('필수항목을 작성해주세요.');
-    //             return;
-    //         } else {
-    //             if (i == canSignup.length-1) {
-    //                 alert('회원가입이 완료되었습니다.');
-    //             }
-    //         }
-    //     }//회원가입 성공 메시지 함수
-    //     //키 비활 활성 기능 필요
-    // });
-    signupCancelButton.addEventListener('click', (e) => {
+    $signupCancelButton.addEventListener('click', (e) => {
         location.href = 'index.html';
     });
 }
+
+function reloadCatImage1() {
+    fetch("https://api.thecatapi.com/v1/images/search?size=full")
+    .then((response) => response.json())
+    .then((data) => data[0].url)
+    .then((catUrl) => $catImage.src = catUrl)
+}
+
+function reloadCatImage2() {
+    axios({
+        method: "get",
+        url: "https://api.thecatapi.com/v1/images/search?size=full",
+        responseType: "json",
+        
+    })
+    .then((response) => {
+        $catImage.src = response.data[0].url;
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+$catImage.addEventListener("click", (e) => {reloadCatImage2()})
+
